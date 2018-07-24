0441c52b6ae92a94e2d271a30cbc15246bcb8178
indianajim <jimrunkel@gmail.com>
2018-07-24 15:43:45 -0400

    initial commit

---
+++ AssetDB/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using AssetDB.Models;
+using System;
+using System.Web.Mvc;
+
+namespace AssetDB.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+            
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
